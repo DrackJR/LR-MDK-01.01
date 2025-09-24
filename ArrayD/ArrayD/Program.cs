55a082a2e991a7d3e2bd6492266d30e87aafacdd
DRACKDOGG <kirillh07@gmail.com>
2025-09-24 23:18:17 +0300

    Создал массив, который можно заполнить из строки

---
+++ ArrayD/ArrayD/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размер массива");
+            int Length = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[Length];
+            for (int i = 0; i < Length; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine();
+            foreach (int i in array)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
