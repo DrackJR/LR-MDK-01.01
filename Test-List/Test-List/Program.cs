ec8e3fe3ec70fcdf4bcccf8df7ee0aba1686fa82
DRACKDOGG <kirillh07@gmail.com>
2025-09-24 23:35:08 +0300

    Создал пустой текстовый список и заполнил его

---
+++ Test-List/Test-List/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Text = new List<string>();
+            Text.Add("One");
+            Text.Add("Two");
+            Text.Add("Three");
+            Text.Add("Four");
+            Text.Add("Five");
+        }
+    }
+}
