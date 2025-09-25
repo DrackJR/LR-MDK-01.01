e03c2f56954dc0b7c86fb71943468b91abc9a5cb
DmtrDv <st56@gmail.com>
2025-09-25 08:22:09 +0300

    Сделал для пользователя возможность ввести длину массива из строки

---
+++ LR 1/work №1/work №1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace work__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину массива");
+            string s = Console.ReadLine();
+            int temp = Convert.ToInt32(s);
+            int[] array = new int[temp];
+
+        }
+    }
+}
