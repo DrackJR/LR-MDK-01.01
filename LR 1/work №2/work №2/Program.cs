a25f1a1e41e2698e3a438138f10308d271f838f9
DmtrDv <st56@gmail.com>
2025-09-25 09:09:04 +0300

    Создал массив из 4000 случайных элементов от 1 до 5000

---
+++ LR 1/work №2/work №2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace work__2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[4000];
+            Random randomValue = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = randomValue.Next(1, 5000);
+            }
+
+            foreach (int elementValue in array)
+            {
+                Console.Write(elementValue + ",");
+            }
+        }
+    }
+}
