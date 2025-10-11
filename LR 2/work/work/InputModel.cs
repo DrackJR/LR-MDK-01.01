cc1fe6392eac5029d1d969a56a012c4f3bca7419
Irina1915 <iryna.budina@yandex.ru>
2025-10-11 12:54:12 +0300

    Создал модуль InputModel и реализовал в нём функции InputServices и InputUserQuery

---
+++ LR 2/work/work/InputModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace work
+{
+    public class InputModel
+    {
+        static public (List<string>[], List<int>[] ) InputServices()
+        {
+            List<string>[] services = new List<string>[3] { new List<string>(),
+                                                                new List<string>(),
+                                                                new List<string>()
+            };
+            List<int>[] count = new List<int>[3] { new List<int>(),
+                                                                new List<int>(),
+                                                                new List<int>()
+            };
+
+            services[0].Add("Стрижка");
+            count[0].Add(3);
+
+            services[1].Add("Массаж лица");
+            count[1].Add(6);
+
+            services[2].Add("Чистка лица");
+            count[2].Add(5);
+
+            return (services, count);
+        }
+
+        static public string InputUserQuery()
+        {
+            Console.Write("Введите, пожалуйста, услугу (Стрижка/Массаж лица/Чистка лица): ");
+            return Console.ReadLine();
+        }
+    }
+}
