416e896c3bef6b73c5c25518a8bc2b4a604e7e5e
DRACKDOGG <kirillh07@gmail.com>
2025-10-17 20:15:19 +0300

    Сделал программу перечисление времени года

---
+++ enum-project/enum-project/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enum_project
+{
+    internal class Program
+    {
+        enum Season
+        {
+            Spring,
+            Summer,
+            Autumn,
+            Winter
+        }
+        static void PrintMessage(Season season)
+        {
+            if (season == Season.Spring)
+            {
+                Console.WriteLine("Сейчас весна");
+            }
+            else if (season == Season.Summer)
+            {
+                Console.WriteLine("Сейчас лето");
+            }
+            else if (season == Season.Autumn)
+            {
+                Console.WriteLine("Сейчас осень");
+            }
+            else
+            {
+                Console.WriteLine("Сейчас зима");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Season now = Season.Autumn;
+
+            PrintMessage(now);
+            PrintMessage(Season.Winter);
+            PrintMessage(Season.Summer);
+        }
+    }
+}
