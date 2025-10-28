f95d693fe7a597d783c95ac0d8ec184b863834dc
Y <@gmail.com>
2025-10-28 10:13:06 +0300

    Сделал вывод всех ключей через запятую

--- DictBestStruct/DictBestStruct/Program.cs
+++ DictBestStruct/DictBestStruct/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,6 +15,8 @@ namespace DictBestStruct
             dict.Add("Шутер", new List<string> {"csgo"});
             dict.Add("Приключение", new List<string> {"Uncharted 4", "The Last Of us"});
             dict.Add("Головоломки", new List<string> { "Machimarium", "Portal 2", "Patrick's Parabox" });
+
+            Console.Write(string.Join(", ", dict.Keys));
         }
     }
 }
