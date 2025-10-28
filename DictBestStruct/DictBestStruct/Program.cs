97de6ca2f18d75eca5bcaf54ff84356ed21b04c8
Y <@gmail.com>
2025-10-28 10:07:30 +0300

    Сделал словарь и заполнил его

---
+++ DictBestStruct/DictBestStruct/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictBestStruct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> dict = new Dictionary<string, List<string>>();
+            dict.Add("Шутер", new List<string> {"csgo"});
+            dict.Add("Приключение", new List<string> {"Uncharted 4", "The Last Of us"});
+            dict.Add("Головоломки", new List<string> { "Machimarium", "Portal 2", "Patrick's Parabox" });
+        }
+    }
+}
