a4c9b89c86a2f597e0da08c2b4e9bd33061ee8d0
DRACKDOGG <kirillh07@gmail.com>
2025-10-28 16:21:27 +0300

    Доделал независимость ввода пользователя жанра от регистра

--- DictBestStruct/DictBestStruct/Program.cs
+++ DictBestStruct/DictBestStruct/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,27 +12,29 @@ namespace DictBestStruct
     {
         static public void ShowCountElement(Dictionary<string, List <string>> dict)
         {
-            Console.WriteLine("Укажите жанр игры(Шутер, Приключение, Головоломки): ");
+            Console.WriteLine("Укажите жанр игры (Шутер, Приключение, Головоломки): ");
             string userQuery = Console.ReadLine().ToLower();
-            List<string> allKeys = new List<string>(dict.Keys);
-            for (int i = 0; i < allKeys.Count; i++)
+            List<string> allKeys = new List<string>();
+            foreach (string key in dict.Keys)
             {
-                allKeys[i].ToLower();
-                Console.WriteLine(i);
+                allKeys.Add(key.ToLower());
+                if (allKeys.Contains(userQuery))
+                {
+                    List<string> value = dict[key];
+                    Console.WriteLine("Число элементов: " + value.Count);
+                    Console.Write(string.Join(", ", value));
+                    return;
+                }
             }
-            if (allKeys.Contains(userQuery))
+            if (!allKeys.Contains(userQuery))
             {
-                List<string> value = dict[userQuery];
-                Console.WriteLine("Число элементов: " + value.Count);
-                Console.Write(string.Join(", ", value));
+                Console.WriteLine("Ай-яй-яй, такого жанра нету");
             }
-            else Console.WriteLine("Ай-яй-яй, такого жанра нету");
-
         }
         static void Main(string[] args)
         {
             Dictionary<string, List<string>> dict = new Dictionary<string, List<string>>();
-            dict.Add("Шутер", new List<string> {"csgo"});
+            dict.Add("Шутер", new List<string> {"cs:go"});
             dict.Add("Приключение", new List<string> {"Uncharted 4", "The Last Of Us"});
             dict.Add("Головоломки", new List<string> {"Machinarium", "Portal 2", "Patrick's Parabox"});
 
