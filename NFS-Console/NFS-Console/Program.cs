1e1cb0d6fbbb70217a3e49076453d90dc3d7f765
D3V1LL-cyber <hiroesmen@gmail.com>
2025-11-22 11:56:23 +0300

    Определил и реализовал функцию LadaVSMerc

--- NFS-Console/NFS-Console/Program.cs
+++ NFS-Console/NFS-Console/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Threading;
 
 namespace NFS_Console
 {
@@ -9,24 +11,52 @@ namespace NFS_Console
         {
             Console.WriteLine(car.GetModel());
         }
+        static public void LadaVSMerc(List<Car> cars)
+        {
+            int cycles = 0;
+            int finish = 100; 
+            while (true)
+            {                
+                foreach (Car auto in cars)
+                {                    
+                  for (int i = 0; i < auto.GetSpeed() / 50 * cycles; i++)
+                  {
+                     Console.Write(" ");
+                        if (auto.GetSpeed() / 50 * cycles >= finish)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Победитель: " + auto.GetModel());
+                            return;
+                        }
+                    }
+                  Console.WriteLine(auto.GetModel());
+                  Console.WriteLine("----------------------------------------------------------------------------------------------------");
+                }
+                Thread.Sleep(1500);
+                Console.Clear();
+                cycles++;
+            }
+        }
         static void Main()
         {
             Car auto = new Car();
             auto.SetModel("Лада");
             auto.SetSpeed(150);
-            Console.WriteLine(auto.GetModel());
+            //Console.WriteLine(auto.GetModel());
             
 
             Car merc = new Car();
             merc.SetModel("Мерс");
-            merc.SetSpeed(300);
+            merc.SetSpeed(600);
 
-            Print(auto);
-            Print(merc);
+            //Print(auto);
+            //Print(merc);
 
             List<Car> cars = new List<Car>();
             cars.Add(auto);
             cars.Add(merc);
+
+            LadaVSMerc(cars);
         }
     }
 }
