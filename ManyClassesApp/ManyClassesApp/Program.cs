06e8b5911232b467751e604ed6bb08652e75b1e0
D3V1LL-cyber <hiroesmen@gmail.com>
2025-11-25 13:24:05 +0300

    Добавил несколько методов для класса Stotage

--- ManyClassesApp/ManyClassesApp/Program.cs
+++ ManyClassesApp/ManyClassesApp/Program.cs
@@ -6,10 +6,35 @@ using System.Threading.Tasks;
 
 namespace ManyClassesApp
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            // Создал новый "в разработке"!
+            Storage storage = new Storage(); 
+            storage.SetIndetifier(123);
+            storage.SetLocation("г. Торжок, Студенческая 3");
+
+            // Создал экземпляр объекта Product и заполнил его данными
+            Product mercedeceBenz = new Product();
+            mercedeceBenz.SetName("Мерседес Бенз");
+            mercedeceBenz.SetPrice(1000000);
+
+            storage.SetProductQuantity(mercedeceBenz, 2);
+
+            // Создал экземпляр объекта Product и заполнил его данными
+            Product uaz = new Product();
+            uaz.SetName("Уазик");
+            uaz.SetPrice(10000000);
+
+            // Добавление 2-х Товара в склад
+            storage.SetProductQuantity(uaz, 5);
+
+            // Вывод продуктов и их количество
+            storage.PrintAllProductsQuantity();
+
+            // Вывод общего количества денег на складе
+            double total = storage.CalcAllMoneyInStorage();
         }
     }
 }
--- ManyClassesApp/ManyClassesApp/Storage.cs
+++ ManyClassesApp/ManyClassesApp/Storage.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
 
 namespace ManyClassesApp
 {
@@ -10,7 +10,7 @@ namespace ManyClassesApp
     {        
         private int identifier_;
         private string location_;
-        private Dictionary<Product, int> products_;
+        private Dictionary<Product, int> products_ = new Dictionary<Product, int>();
 
         // Метод установки идентификатора
         public void SetIndetifier(int identifier)
@@ -28,6 +28,27 @@ namespace ManyClassesApp
         public void SetProductQuantity(Product product, int quantity)
         {
             products_.Add(product, quantity);
+        }
+
+        // Метод вывода на консоль всех товаров и их количества
+        public void PrintAllProductsQuantity()
+        {
+            foreach(Product key in products_.Keys)
+            {
+                Console.WriteLine(key.GetName() + " " + products_[key] + " шт.");
+            }
+        }
+
+        // Метод Вывода общего количества денег на консоль
+        public double CalcAllMoneyInStorage()
+        {
+            double total = 0;
+            foreach(Product key in products_.Keys)
+            {
+                double cost = key.GetPrice() * products_[key];
+                total += cost;
+            }
+            return total;
         }
     }
 }
