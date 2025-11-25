14f326da166a0f7d4bbc1cc6e71d11f183fdcc9e
D3V1LL-cyber <hiroesmen@gmail.com>
2025-11-25 12:23:15 +0300

    Создал класс Product и описал его поля

---
+++ ManyClassesApp/ManyClassesApp/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManyClassesApp
+{
+    public class Product
+    {
+        private string name_;
+        private double price_;
+        
+    }
+}
