5d768d5ed970da9e9f433b9dffc4a22ae97409d7
D3V1LL-cyber <hiroesmen@gmail.com>
2025-11-25 12:26:17 +0300

    Создал 4 метода для работы с классом

--- ManyClassesApp/ManyClassesApp/Product.cs
+++ ManyClassesApp/ManyClassesApp/Product.cs
@@ -11,6 +11,28 @@ namespace ManyClassesApp
     {
         private string name_;
         private double price_;
-        
+
+        // Метод установки имени товара
+        public void SetName(string name)
+        {
+            name_ = name;
+        }
+
+        // Метод получения имени товара
+        public string GetName()
+        {
+            return name_;
+        }
+        // Метод установки цены товара
+        public void SetPrice(double price)
+        {
+            price_ = price;
+        }
+
+        // Метод получения цены товара
+        public double GetPrice()
+        {
+            return price_;
+        }
     }
 }
