3601947274a8926e83524b82804710ee3e9cebaf
DRACKDOGG <kirillh07@gmail.com>
2025-11-25 11:44:01 +0300

    Создал матрицу 2 на 3 и вывел её на консоль

--- MatrixDZ/MatrixDZ/Program.cs
+++ MatrixDZ/MatrixDZ/Program.cs
@@ -18,6 +18,18 @@ namespace MatrixDZ
             matrix_2x2.SetValueInMatrix(1, 1, 4);
             matrix_2x2.Print();
             Console.WriteLine("Определитель квадратной матрицы = " + matrix_2x2.CalcDeterminant());
+
+            Console.WriteLine();
+
+            Matrix matrix_2x3 = new Matrix();
+            matrix_2x3.CreateMatrix(2, 3);
+            matrix_2x3.SetValueInMatrix(0, 0, 1);
+            matrix_2x3.SetValueInMatrix(0, 1, 2);
+            matrix_2x3.SetValueInMatrix(0, 2, 3);
+            matrix_2x3.SetValueInMatrix(1, 0, 4);
+            matrix_2x3.SetValueInMatrix(1, 1, 5);
+            matrix_2x3.SetValueInMatrix(1, 2, 6);
+            matrix_2x3.Print();
         }
     }
 }
