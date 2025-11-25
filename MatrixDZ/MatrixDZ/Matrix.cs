c71abb279bd33b34c9035388be7027202f4c933e
DRACKDOGG <kirillh07@gmail.com>
2025-11-25 11:40:35 +0300

    Создал метод CreateMatrix

--- MatrixDZ/MatrixDZ/Matrix.cs
+++ MatrixDZ/MatrixDZ/Matrix.cs
@@ -7,5 +7,14 @@ namespace MatrixDZ
         private int[,] values_;
         private int rows_;
         private int columns_;
+
+
+        // Метод создания матрицы
+        public void CreateMatrix(int rows, int columns)
+        {
+            rows_ = rows;
+            columns_ = columns;
+            values_ = new int[rows_, columns_];
+        }
     }
 }
