878669f60072c42956651b729d81702e5442244b
DRACKDOGG <kirillh07@gmail.com>
2025-11-25 11:40:56 +0300

    Создал метод SetValueInMatrix

--- MatrixDZ/MatrixDZ/Matrix.cs
+++ MatrixDZ/MatrixDZ/Matrix.cs
@@ -16,5 +16,11 @@ namespace MatrixDZ
             columns_ = columns;
             values_ = new int[rows_, columns_];
         }
+
+        // Метод установки значения в ячейку матрицу
+        public void SetValueInMatrix(int row, int column, int value)
+        {
+            values_[row, column] = value;
+        }
     }
 }
