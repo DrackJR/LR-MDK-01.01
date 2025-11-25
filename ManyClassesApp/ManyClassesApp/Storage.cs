b28c5b84f2754104ac4cad01c33d162088a357c8
D3V1LL-cyber <hiroesmen@gmail.com>
2025-11-25 12:47:29 +0300

    Сделал методы для класса Storage для установки идентификатора, местоположения и товара и его количества

--- ManyClassesApp/ManyClassesApp/Storage.cs
+++ ManyClassesApp/ManyClassesApp/Storage.cs
@@ -11,5 +11,23 @@ namespace ManyClassesApp
         private int identifier_;
         private string location_;
         private Dictionary<Product, int> products_;
+
+        // Метод установки идентификатора
+        public void SetIndetifier(int identifier)
+        {
+            identifier_ = identifier;
+        }
+
+        // Метод установки местоположения
+        public void SetLocation(string location)
+        {
+            location_ = location;
+        }
+
+        // Метод установки товара и его количества
+        public void SetProductQuantity(Product product, int quantity)
+        {
+            products_.Add(product, quantity);
+        }
     }
 }
