c035f38623bb5e65b9756ec18789a1620868b9fd
D3V1LL-cyber <hiroesmen@gmail.com>
2026-01-23 08:17:34 +0300

    добавил новый проект и почти доделанный проект

---
+++ Кораблик/Кораблик/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Кораблик
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            // Объявляем объект "g" класса Graphics и предоставляем
+            // ему возможность рисования на pictureBox1:
+            Graphics g = pictureBox1.CreateGraphics();
+            g.Clear(Color.BlueViolet);
+            // Создаем объекты-кисти для закрашивания фигур
+            SolidBrush myTrum = new SolidBrush(Color.DarkGray);
+            SolidBrush myTrub = new SolidBrush(Color.DeepPink);
+            //Выбираем перо myPen желтого цвета толщиной в 2 пикселя:
+            Pen myWind = new Pen(Color.Green, 2);
+            // Закрашиваем фигуры
+            g.FillRectangle(myTrub, 300, 125, 75, 75); // 1 труба (прямоугольник)
+            g.FillRectangle(myTrub, 480, 125, 75, 75); // 2 труба (прямоугольник)
+           
+            g.FillRectangle(myTrum, 250, 200, 350, 100); // палуба (прямоугольник)
+                                                         // Море - 12 секторов-полуокружностей
+            int x = 50;
+            int Radius = 50;
+            while (x <= pictureBox1.Width - Radius)
+            {
+                
+            }
+            // Иллюминаторы 
+            for (int y = 300; y <= 550; y += 50)
+            {
+                g.DrawEllipse(myWind, y, 240, 20, 20); // 6 окружностей
+            }
+        }
+    }
+}
