d43bd55d0bb4c8e196c364b650a15acb2533ccc2
D3V1LL-cyber <hiroesmen@gmail.com>
2026-01-30 11:26:53 +0300

    переделать работу

---
+++ LR 3 Variant5/LR 3 Variant5/Dish.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR_3_Variant5
+{
+    public class Dish
+    {
+        private string name_;
+        private double price_;
+        private string photo_;
+        private string description_;
+
+        public Dish() 
+        {
+
+        }
+
+        public Dish(string name, double price, string photo, string description)
+        {
+            name_ = name;
+            price_ = price;
+            photo_ = photo;
+            description_ = description;
+        }
+
+        public string Name
+        {
+            get { return name_; }
+        }
+
+
+        public double Price
+        {
+            get { return price_; }
+        }
+
+        public string Description
+        {
+            get { return description_; }
+        }
+
+        public string Photo
+        {
+            get { return photo_; }
+        }
+    }
+}
--- LR 3 Variant5/LR 3 Variant5/MainForm.Designer.cs
+++ LR 3 Variant5/LR 3 Variant5/MainForm.Designer.cs
@@ -35,6 +35,9 @@
             this.QuantityOfOrderTextBox = new System.Windows.Forms.TextBox();
             this.OrderButton = new System.Windows.Forms.Button();
             this.DescriptionLabel = new System.Windows.Forms.Label();
+            this.PriceInfoLabel = new System.Windows.Forms.Label();
+            this.PriceLabel = new System.Windows.Forms.Label();
+            this.DescriptionInfoLabel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.PhotoOfDishPictureBox)).BeginInit();
             this.SuspendLayout();
             // 
@@ -46,6 +49,7 @@
             this.GroupDishListBox.Name = "GroupDishListBox";
             this.GroupDishListBox.Size = new System.Drawing.Size(98, 450);
             this.GroupDishListBox.TabIndex = 0;
+            this.GroupDishListBox.SelectedIndexChanged += new System.EventHandler(this.GroupDishListBox_SelectedIndexChanged);
             // 
             // DishMenuComboBox
             // 
@@ -55,6 +59,7 @@
             this.DishMenuComboBox.Name = "DishMenuComboBox";
             this.DishMenuComboBox.Size = new System.Drawing.Size(121, 21);
             this.DishMenuComboBox.TabIndex = 1;
+            this.DishMenuComboBox.SelectedIndexChanged += new System.EventHandler(this.DishMenuComboBox_SelectedIndexChanged);
             // 
             // PhotoOfDishPictureBox
             // 
@@ -68,7 +73,7 @@
             // 
             this.ExplanationLabel.AutoSize = true;
             this.ExplanationLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.ExplanationLabel.Location = new System.Drawing.Point(113, 209);
+            this.ExplanationLabel.Location = new System.Drawing.Point(113, 314);
             this.ExplanationLabel.Name = "ExplanationLabel";
             this.ExplanationLabel.Size = new System.Drawing.Size(148, 20);
             this.ExplanationLabel.TabIndex = 3;
@@ -76,7 +81,7 @@
             // 
             // QuantityOfOrderTextBox
             // 
-            this.QuantityOfOrderTextBox.Location = new System.Drawing.Point(137, 241);
+            this.QuantityOfOrderTextBox.Location = new System.Drawing.Point(136, 349);
             this.QuantityOfOrderTextBox.Name = "QuantityOfOrderTextBox";
             this.QuantityOfOrderTextBox.Size = new System.Drawing.Size(100, 20);
             this.QuantityOfOrderTextBox.TabIndex = 4;
@@ -84,7 +89,7 @@
             // OrderButton
             // 
             this.OrderButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.OrderButton.Location = new System.Drawing.Point(136, 149);
+            this.OrderButton.Location = new System.Drawing.Point(136, 384);
             this.OrderButton.Name = "OrderButton";
             this.OrderButton.Size = new System.Drawing.Size(101, 43);
             this.OrderButton.TabIndex = 5;
@@ -95,11 +100,41 @@
             // 
             this.DescriptionLabel.AutoSize = true;
             this.DescriptionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.DescriptionLabel.Location = new System.Drawing.Point(484, 384);
+            this.DescriptionLabel.Location = new System.Drawing.Point(333, 384);
             this.DescriptionLabel.Name = "DescriptionLabel";
-            this.DescriptionLabel.Size = new System.Drawing.Size(51, 20);
+            this.DescriptionLabel.Size = new System.Drawing.Size(83, 20);
             this.DescriptionLabel.TabIndex = 3;
-            this.DescriptionLabel.Text = "label1";
+            this.DescriptionLabel.Text = "Описание";
+            // 
+            // PriceInfoLabel
+            // 
+            this.PriceInfoLabel.AutoSize = true;
+            this.PriceInfoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.PriceInfoLabel.Location = new System.Drawing.Point(132, 244);
+            this.PriceInfoLabel.Name = "PriceInfoLabel";
+            this.PriceInfoLabel.Size = new System.Drawing.Size(87, 20);
+            this.PriceInfoLabel.TabIndex = 3;
+            this.PriceInfoLabel.Text = "Цена (руб)";
+            // 
+            // PriceLabel
+            // 
+            this.PriceLabel.AutoSize = true;
+            this.PriceLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.PriceLabel.Location = new System.Drawing.Point(162, 277);
+            this.PriceLabel.Name = "PriceLabel";
+            this.PriceLabel.Size = new System.Drawing.Size(48, 20);
+            this.PriceLabel.TabIndex = 3;
+            this.PriceLabel.Text = "Цена";
+            // 
+            // DescriptionInfoLabel
+            // 
+            this.DescriptionInfoLabel.AutoSize = true;
+            this.DescriptionInfoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.DescriptionInfoLabel.Location = new System.Drawing.Point(469, 349);
+            this.DescriptionInfoLabel.Name = "DescriptionInfoLabel";
+            this.DescriptionInfoLabel.Size = new System.Drawing.Size(83, 20);
+            this.DescriptionInfoLabel.TabIndex = 3;
+            this.DescriptionInfoLabel.Text = "Описание";
             // 
             // MainForm
             // 
@@ -108,7 +143,10 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.OrderButton);
             this.Controls.Add(this.QuantityOfOrderTextBox);
+            this.Controls.Add(this.DescriptionInfoLabel);
             this.Controls.Add(this.DescriptionLabel);
+            this.Controls.Add(this.PriceLabel);
+            this.Controls.Add(this.PriceInfoLabel);
             this.Controls.Add(this.ExplanationLabel);
             this.Controls.Add(this.PhotoOfDishPictureBox);
             this.Controls.Add(this.DishMenuComboBox);
@@ -130,6 +168,9 @@
         private System.Windows.Forms.TextBox QuantityOfOrderTextBox;
         private System.Windows.Forms.Button OrderButton;
         private System.Windows.Forms.Label DescriptionLabel;
+        private System.Windows.Forms.Label PriceInfoLabel;
+        private System.Windows.Forms.Label PriceLabel;
+        private System.Windows.Forms.Label DescriptionInfoLabel;
     }
 }
 
